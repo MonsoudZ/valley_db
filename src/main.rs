9613425d4714d3fc0608902555f3c5fa0c3776ea
MonsoudZ <monsoud.zanaty@gmail.com>
2026-08-13 12:32:02 -0600

    finished error handling

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
             .read_line(&mut user_action)
             .expect("Not a valid function call");
 
-        let user_action = user_action.trim();
+        let user_action = user_action.trim().to_lowercase();
 
         if user_action == "insert" {
             insert(&mut mapping)
@@ -26,6 +26,9 @@ fn main() {
         else if user_action == "exit" {
             break;
         }
+        else {
+            println!("Unknown action");
+        }
     }
 }
 
@@ -34,7 +37,12 @@ fn insert(i: &mut HashMap<String, String>) {
 }
 
 fn delete_from_value(i: &mut HashMap<String, String>) {
-i.remove(&(get_key()));
+    match i.remove(&(get_key())) {
+        Some(value) => {
+            println!("Value: {value} was deleted");
+        }
+        None => println!("No key found, try again."),
+    }
 }
 
 fn get_value_for(i: &HashMap<String, String>) {
@@ -55,22 +63,40 @@ fn get_value_for(i: &HashMap<String, String>) {
 }
 
 fn get_key() -> String {
-    println!("Input key name");
+    loop {
+        println!("Input key name");
+        let mut key = String::new();
 
-    let mut key = String::new();
-    io::stdin()
-        .read_line(&mut key)
-        .expect("Failed to read line");
-    key.trim().to_string()
+        io::stdin()
+            .read_line(&mut key)
+            .expect("Failed to read line");
+
+        let cleaned_key = key.trim();
+
+        if cleaned_key.is_empty() {
+            println!("empty key, try again.");
+        } else {
+            return cleaned_key.to_string();
+        }
+    }
 }
 fn get_value() -> String {
-    println!("Input Value");
+    loop {
+        println!("Input Value");
+        let mut value = String::new();
 
-    let mut value = String::new();
-    io::stdin()
-        .read_line(&mut value)
-        .expect("Failed to read line");
-    value.trim().to_string()
+        io::stdin()
+            .read_line(&mut value)
+            .expect("Failed to read line");
+
+         let cleaned_value = value.trim();
+
+        if cleaned_value.is_empty() {
+            println!("empty value, try again.");
+        } else {
+            return cleaned_value.to_string();
+        }
+    }
 }
 
 
